2a49fe93b47276085eda1b92c9daee91047ec482
Vidura Prasangana <vidura.prasangana16@gmail.com>
2022-02-02 23:59:31 +0530

    data loader

---
+++ data.py
@@ -0,0 +1,105 @@
+import torch
+from torch.utils.data import Dataset , Sampler
+from PIL import Image
+import os
+import copy
+from typing import Iterator, Optional, Sequence, List, TypeVar, Generic, Sized
+
+
+class ContrastiveDataset(Dataset):
+    def __init__(self, data_dir, image_list_file, transform=None):
+        """
+        Args:
+            data_dir: path to image directory.
+            image_list_file: path to the file containing images
+                with corresponding labels.
+            transform: optional transform to be applied on a sample.
+        """
+        image_names = []
+        labels = []
+        with open(image_list_file, "r") as f:
+            for line in f:
+                items = line.split()
+                image_name= items[0]
+                label = items[1:]
+                label = [int(i) for i in label]
+                image_name = os.path.join(data_dir, image_name)
+                image_names.append(image_name)
+                labels.append(label)
+
+        self.image_names = image_names
+        self.labels = labels
+        self.transform = transform
+
+    def __getitem__(self, index):
+        """
+        Args:
+            index: the index of item
+
+        Returns:
+            image and its labels
+        """
+        image_name = self.image_names[index]
+        image = Image.open(image_name).convert('RGB')
+        label = self.labels[index]
+        if self.transform is not None:
+            image = self.transform(image)
+        return image, torch.FloatTensor(label)
+
+    def __len__(self):
+        return len(self.image_names)
+
+class ContrastiveBatchSampler(Sampler[List[int]]):
+    r"""Wraps another sampler to yield a mini-batch of indices.
+
+    Args:
+        sampler (Sampler or Iterable): Base sampler. Can be any iterable object
+        batch_size (int): Size of mini-batch.
+        drop_last (bool): If ``True``, the sampler will drop the last batch if
+            its size would be less than ``batch_size``
+
+    Example:
+        >>> list(BatchSampler(SequentialSampler(range(10)), batch_size=3, drop_last=False))
+        [[0, 1, 2], [3, 4, 5], [6, 7, 8], [9]]
+        >>> list(BatchSampler(SequentialSampler(range(10)), batch_size=3, drop_last=True))
+        [[0, 1, 2], [3, 4, 5], [6, 7, 8]]
+    """
+
+    def __init__(self, sampler: Sampler[int], batch_size: int, drop_last: bool) -> None:
+        # Since collections.abc.Iterable does not check for `__getitem__`, which
+        # is one way for an object to be an iterable, we don't do an `isinstance`
+        # check here.
+        if not isinstance(batch_size, int) or isinstance(batch_size, bool) or \
+                batch_size <= 0:
+            raise ValueError("batch_size should be a positive integer value, "
+                             "but got batch_size={}".format(batch_size))
+        if not isinstance(drop_last, bool):
+            raise ValueError("drop_last should be a boolean value, but got "
+                             "drop_last={}".format(drop_last))
+        self.sampler = sampler
+        self.batch_size = batch_size
+        self.drop_last = drop_last
+        self.samplers = [sampler]
+        for _ in range(batch_size-1):
+            self.samplers.append(copy.deepcopy(sampler))
+
+    def __iter__(self) -> Iterator[List[int]]:
+        batch = []
+        for sampler in self.samplers:
+            batch.append(next(sampler))
+        yield batch
+
+    def __len__(self) -> int:
+        # Can only be called if self.sampler has __len__ implemented
+        # We cannot enforce this condition, so we turn off typechecking for the
+        # implementation below.
+        # Somewhat related: see NOTE [ Lack of Default `__len__` in Python Abstract Base Classes ]
+
+        t_len = 0
+        for s in self.samplers:
+            t_len += len(s)
+        return t_len
+        # if self.drop_last:
+        #     return len(self.sampler) // self.batch_size  # type: ignore[arg-type]
+        # else:
+        #     return (len(self.sampler) + self.batch_size - 1) // self.batch_size  # type: ignore[arg-type]

db57e8c9e962efe9f85eb3974f8deb1d5da7d8b1
Vidura Prasangana <vidura.prasangana16@gmail.com>
2022-02-05 15:41:34 +0530

    paramerter modify

--- run.py
+++ run.py
@@ -14,8 +14,8 @@ model_names = sorted(name for name in models.__dict__
 parser = argparse.ArgumentParser(description='PyTorch SimCLR')
 parser.add_argument('-data', metavar='DIR', default='./datasets',
                     help='path to dataset')
-parser.add_argument('-dataset-name', default='stl10',
-                    help='dataset name', choices=['stl10', 'cifar10'])
+parser.add_argument('-dataset-name', default='contrastive',
+                    help='dataset name', choices=['stl10', 'cifar10','contrastive'])
 parser.add_argument('-a', '--arch', metavar='ARCH', default='resnet18',
                     choices=model_names,
                     help='model architecture: ' +
@@ -51,7 +51,6 @@ parser.add_argument('--temperature', default=0.07, type=float,
 parser.add_argument('--n-views', default=2, type=int, metavar='N',
                     help='Number of views for contrastive learning training.')
 parser.add_argument('--gpu-index', default=0, type=int, help='Gpu index.')
-parser.add_argument('--dataset_name', default="contrastive", type=int, help='Dataset')
 
 
 def main():

bcd4f3bd85953f7e3abc052cfb88b51b444e7451
Vidura Prasangana <vidura.prasangana16@gmail.com>
2022-02-09 01:51:38 +0530

    est

--- simclr.py
+++ simclr.py
@@ -167,8 +167,8 @@ class SimCLR(object):
             print(epoch_reslts)
             wandb.log(epoch_reslts)
             # warmup for the first 10 epochs
-            if epoch_counter >= 10:
-                self.scheduler.step()
+            # if epoch_counter >= 10:
+            #     self.scheduler.step()
             # logging.debug(f"Epoch: {epoch_counter}\tLoss: {loss}\tTop1 accuracy: {top1[0]}")
 
         self.save_json(complete_reslts, 'training_logs')
